8d1f1acb1957fef69352255fe7ca09f293635b60
BrianNatanaelWilbers <briannatanaelwilbers@gmail.com>
2026-09-12 11:03:42 -0300

    Update EfCoreBookAppService_Tests.cs

--- test/SmartPantry.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Books/EfCoreBookAppService_Tests.cs
+++ test/SmartPantry.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Books/EfCoreBookAppService_Tests.cs
@@ -10,30 +10,4 @@ namespace SmartPantry.EntityFrameworkCore.Applications.Books;
 [Collection(SmartPantryTestConsts.CollectionDefinitionName)]
 public class EfCoreBookAppService_Tests : BookAppService_Tests<SmartPantryEntityFrameworkCoreTestModule>
 {
-    [Fact]
-    public async Task Should_Create_A_Valid_Book()
-    {
-        // Arrange: Crear un Author primero
-        var authorRepository = GetRequiredService<IRepository<Author, Guid>>();
-        var author = new Author
-        {
-            Name = "Test Author"
-        };
-        await authorRepository.InsertAsync(author);
-        await CurrentUnitOfWork.SaveChangesAsync();
-
-        // Act: Crear el Book con el Author válido
-        var input = new CreateBookDto
-        {
-            Title = "Test Book",
-            AuthorId = author.Id
-        };
-
-        var result = await BookAppService.CreateAsync(input);
-
-        // Assert
-        result.ShouldNotBeNull();
-        result.Title.ShouldBe("Test Book");
-        result.AuthorId.ShouldBe(author.Id);
-    }
 }
