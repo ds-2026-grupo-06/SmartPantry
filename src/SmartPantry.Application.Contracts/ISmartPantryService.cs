d219daf5066cf273007d645dd9e609cb8b6fb6be
BrianNatanaelWilbers <briannatanaelwilbers@gmail.com>
2026-09-19 09:44:59 -0300

    Cambios en Services
    Se modifico ISmartPantryService y SmartPantryService debido a un error en la definición de los atributos de product, lo que causo un error en Backend

--- src/SmartPantry.Application.Contracts/ISmartPantryService.cs
+++ src/SmartPantry.Application.Contracts/ISmartPantryService.cs
@@ -8,7 +8,11 @@ namespace SmartPantry;
 public interface ISmartPantryService : IApplicationService
 {
     Task<List<ProductDto>> GetListAsync();
-    Task<ProductDto> CreateAsync(string text);
+    Task<ProductDto> CreateAsync(string barcode);
+    Task<ProductDto> CreateAsync(string name);
+    Task<ProductDto> CreateAsync(string brand);
+    Task<ProductDto> CreateAsync(string nutriScore);
+    Task<ProductDto> CreateAsync(int novaGroup);
     Task DeleteAsync(Guid id);
 }
 
